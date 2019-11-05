edbfb5bd4be1cd8296168bac6910f8dc8eca6304
Hector-bit <hectormartinez.2978@gmail.com>
2019-11-05 00:00:33 -0800

    CRUD operations are done

--- data/customers/customers-route.js
+++ data/customers/customers-route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const knex = require('../dbConfig');
+const db = require('../dbConfig');
 
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    knex
+    db
     .select('*')
     .from('accounts')
     .then(customer => {
@@ -18,14 +18,52 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    knew
+    db
     .select('*')
     .from('accounts')
+    .where( 'id', '=', req.params.id)
     .then(account => {
         res.status(200).json(account)
     })
     .catch(error => {
         res.status(404).json({ error: 'could not display account'})
+    })
+})
+
+router.post('/', (req, res) => {
+    db
+    .insert(req.body)
+    .into('accounts')
+    .then( account => {
+        res.status(200).json('account was added');
+    })
+    .catch(error => {
+        res.status(400).json({ error: 'could not post account'})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    db('accounts')
+    .where('id', '=', req.params.id)
+    .del()
+    .then(account => {
+        res.status(200).json('account was deleted');
+    })
+    .catch(error => {
+        res.status(400).json({ error: 'could not delete account'})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    db
+    .update(req.body)
+    .into('accounts')
+    .where('id', '=', req.params.id)
+    .then(account => {
+        res.status(200).json('account was updated');
+    })
+    .catch(error => {
+        res.status(400).json({ error: 'could not update account'})
     })
 })
 
